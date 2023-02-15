383a1307e9f5793984352ca4af84ac9db63ceab2
Shahmirze <sahmirze.memmedyarov@gmail.com>
2023-02-15 23:47:59 +0400

    ready  grid 1

--- raect_routers/src/pages/home.jsx
+++ raect_routers/src/pages/home.jsx
@@ -2,31 +2,58 @@
 import PageContainer from "routes/PageContainer"
 import Frame from 'assist/Frame.svg'
 import Shape1 from 'assist/shape-1.svg'
+import Group from 'assist/Group.svg'
 import { Container } from "@mui/system"
-import { Grid } from "@mui/material"
+import { Box, CardMedia, Grid, Typography } from "@mui/material"
 function Home(){
 	return(
 		<>	
 			<PageContainer >
-				<Container    sx={{
+				<Container     sx={{
 					minHeight:'80vh',
-					
 				}}>
 					<Grid container spacing={2}>
-						<Grid item  sm={3}  sx={{
-							backgroundColor:'red'
+						<Grid item  direction={"column"} sm={3}  sx={{
+							backgroundColor:'red',
 							}}>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ad id tenetur assumenda incidunt laudantium laboriosam cupiditate dolor, nulla nisi voluptatem dolore animi modi eos itaque ex debitis veritatis iusto perferendis aut facere! Iusto autem molestiae beatae exercitationem id, omnis consectetur recusandae aperiam qui non commodi culpa ipsum voluptatem nisi optio nihil! Repudiandae voluptatum, esse minus quisquam corrupti cumque! Reiciendis et vitae impedit aperiam ex ducimus libero! Explicabo aspernatur aut fugit quia voluptatum autem, quae ad cupiditate dicta atque totam, cum doloremque, nam iusto cumque asperiores velit non sed possimus ducimus a laborum? Iste non dolorem sunt, dolorum obcaecati quisquam?
+							<Box sx={{
+								width:'40%'
+							}}>
+								<CardMedia
+							 component="img"
+							 weigth="20"
+							 height=""
+							 image={Shape1}
+							>
+							</CardMedia>
+							</Box>
+							<Box  sx={{
+								width:'40%',
+							}}>
+							<CardMedia
+							 component="img"
+						
+							 image={Frame}
+							>
+							</CardMedia>
+							</Box>
 						</Grid>
 						<Grid item  sm={6} sx={{
 							backgroundColor:'blue'
 						}}>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, ab. Dicta, asperiores laborum cum dolorem placeat doloremque distinctio eos ipsam aliquam nulla aperiam sapiente nemo unde sequi beatae vitae omnis recusandae explicabo temporibus dolor labore accusamus. Amet ut repellendus et. Perspiciatis, molestias beatae, earum tempora blanditiis quae adipisci minus aliquam cumque, vero magni iusto. Ab, blanditiis, laborum mollitia a nesciunt architecto ullam ex, debitis in labore itaque dicta culpa doloribus vel ipsam possimus quas voluptatibus earum iste inventore reprehenderit excepturi expedita. Exercitationem commodi, molestiae aut expedita illum praesentium culpa deleniti sequi nulla cupiditate facere, iusto rerum fugiat suscipit ratione ad consectetur saepe nostrum vero, sunt laboriosam quas eum at! Excepturi quae porro vero ab harum sequi provident? Animi itaque dolores at veritatis quam ab, excepturi similique ipsam repellendus numquam natus nostrum facere possimus perspiciatis ratione esse tempora ex nemo omnis voluptates optio. Quo ipsum iure atque eligendi laborum culpa, omnis dolore repellat voluptatum, ducimus dolor ab, tempore voluptatem accusamus facere labore officia magni nihil aliquam cum eius iusto fuga? Laboriosam deleniti eveniet explicabo adipisci vel quasi nobis ad dolor, sunt eius aliquid dolore, magni maiores minus, voluptatum quod maxime? Assumenda eos illo at cumque. Suscipit quaerat voluptates dolore consectetur libero!
+							<Typography variant="h4" align="center">
+								Daxil Olun
+							</Typography>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, dolore. Ad aliquid iure rerum consectetur voluptatibus delectus culpa quod soluta maiores atque vel facere expedita omnis, ducimus, accusantium dolor, ratione beatae. Dolores excepturi ducimus magnam maiores accusamus, labore, nam atque aut eos nisi odio at reiciendis impedit. Autem rem quaerat facere minima veritatis consequatur labore placeat mollitia, nisi quibusdam animi illum rerum pariatur id ratione suscipit beatae molestias consectetur magnam sapiente veniam adipisci amet modi vero! Ea ullam in quisquam harum dolorem vitae tempore perferendis temporibus sunt dignissimos accusamus quaerat, delectus magnam iusto! Natus soluta saepe quos sapiente ex qui.
 						</Grid>
 						<Grid item  sm={3} sx={{
 							backgroundColor:'yellow'
 						}}>
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quo sapiente autem deleniti, saepe sequi asperiores incidunt id eum pariatur reiciendis enim laboriosam magni, ullam quia cum! Quia quos dolorum illum id nobis dignissimos quibusdam recusandae impedit. Omnis quod incidunt voluptates dolorum officiis a perspiciatis minus. Est consectetur minus ullam excepturi tempore temporibus, aspernatur totam harum cupiditate adipisci, ex, odit ut autem ipsa facilis nobis voluptatibus maiores earum nam quas consequatur aliquam ab unde. Aut reprehenderit qui magnam dolor minus, aspernatur eaque sit quis impedit obcaecati hic! Commodi mollitia est voluptatum, vero nesciunt alias aperiam minus ipsam optio debitis autem, repellendus nisi? Fugit recusandae delectus impedit ipsum id, ratione dicta ipsam accusantium asperiores mollitia illo optio illum repudiandae provident ducimus assumenda, beatae minus cupiditate! Harum amet eos eaque provident laborum deserunt sed ex labore quibusdam. Asperiores vitae enim nisi voluptatibus deserunt obcaecati sit voluptatum. Expedita aspernatur perspiciatis sunt eos maxime.
+							<item>
+								<img src={Group} alt="" />
+								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum laborum eius, fuga odit inventore odio commodi dolorum voluptatum similique dicta reprehenderit deserunt nobis obcaecati porro quis unde nostrum delectus eaque non itaque id? Animi assumenda sint iure perspiciatis veniam illum ea nam officiis esse sit amet repellendus id quam molestiae dolore sequi cupiditate, porro, soluta nostrum. Unde quod impedit sint distinctio, nostrum tenetur blanditiis ut incidunt voluptatum debitis voluptates natus consectetur enim repudiandae, ipsam aut rerum minus dolorem modi nisi sapiente perferendis non. Magnam ipsam recusandae ipsum perspiciatis beatae! Rem animi blanditiis ipsum nihil, voluptatibus nobis delectus autem suscipit. Laborum.
+							</item>
+							
 						</Grid>
 
 					</Grid>
